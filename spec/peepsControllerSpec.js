145572113258fd058c38fcf6612d79dc933b9f0e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-30 23:25:52 +0100

    Refactor XMLHTTPRequest out of index
    * Move all peeps API call into the peeps controller

--- spec/peepsControllerSpec.js
+++ spec/peepsControllerSpec.js
@@ -12,3 +12,20 @@
   assert.isTrue(element === htmlText)
   console.log("Test: Peeps Controller renders peep on the page PASSED")
 })();
+
+// (function testPeepsControllerLoadsPeepsFromAPICall() {
+//     var peepListViewDouble = { peepListArray: [{"id":1, "body":"Test chitter peep number 4","created_at":"2018-06-23T13:21:23.317Z","updated_at":"2018-06-23T13:21:23.317Z","user":{"id":1,"handle":"test_user"},"likes":[{"user":{"id":2,"handle":"test_user2"}}]}]};
+//     var peepsController = new PeepsController(peepListViewDouble);
+//     // var apiCallDouble = [{"id":1, "body":"Test chitter peep number 4","created_at":"2018-06-23T13:21:23.317Z","updated_at":"2018-06-23T13:21:23.317Z","user":{"id":1,"handle":"test_user"},"likes":[{"user":{"id":2,"handle":"test_user2"}}]}]
+//     var htmlText = "<ul><li><div>Test chitter peep number 4</div><div>Posted by: test_user</div></li><br></ul>";
+//     // peepListViewDouble.addPeeps(apiCallDouble) = function stubPeepsCallback() {
+//     //   return htmlText;
+//     // };
+//     peepsController.loadPeeps = function stubAPICAll() {
+//       return htmlText;
+//     }
+//     var element = document.getElementById('app').innerHTML;
+//     console.log(element)
+//     assert.isTrue(element === htmlText);
+//     console.log("Test for: Peep Controller renders XML response PASSED");
+// })();
--- src/peepsController.js
+++ src/peepsController.js
@@ -11,5 +11,31 @@
   element.innerHTML = this.peepsListView.returnPeepHTML();
   };
 
+  PeepsController.prototype.loadPeeps = function() {
+    var xhr = new XMLHttpRequest();
+    var url = "https://chitter-backend-api.herokuapp.com/peeps"
+    xhr.onreadystatechange = peepsCallback;
+    function peepsCallback() {
+      if (xhr.readystate < 4) {
+        return;
+      }
+      if (xhr.status !== 200) {
+        return;
+      }
+      var apiCall = JSON.parse(xhr.responseText);
+
+      executePeeps(apiCall);
+    };
+    xhr.open("GET", url, true);
+    xhr.send("");
+  }
+
+  function executePeeps(apiCall) {
+    var peepsList = new PeepList;
+    peepsList.addPeeps(apiCall);
+    var peepsController = new PeepsController(peepsList);
+    peepsController.renderHTML();
+  };
+
   exports.PeepsController = PeepsController;
 })(this);
